7173dbb90464cc99a6b58d64d6b2119079dd74eb
azpeteryang <azhenyuyang@outlook.com>
2020-05-29 10:55:46 -0400

    Add files via upload

---
+++ Divide_Conquer/315.Count-of-Smaller-Numbers-After-Self/315.Count of Smaller Numbers After Self.cpp
@@ -0,0 +1,36 @@
+class Solution {    
+public:
+    vector<int> countSmaller(vector<int>& nums) 
+    {        
+        int N = nums.size();
+        if (N==0) return {};
+        
+        vector<int>sortedNums = nums;        
+        vector<int>counts(N,0);
+        DivideConque(nums,sortedNums, counts, 0, nums.size()-1);  // 0表示起点，N-1表示终点
+        return counts;
+    }
+    
+    void DivideConque(vector<int>&nums, vector<int>&sortedNums, vector<int>&counts, int start, int end)
+    {
+        if (start==end) return;
+        
+        // 如果需要处理的元素有多个，则折半分治处理。注意：分治处理后的sortedNums前后两部分元素都是有序的。
+        int mid = start+(end-start)/2;
+        DivideConque(nums, sortedNums, counts, start,mid);
+        DivideConque(nums, sortedNums, counts, mid+1, end);
+        
+        // OK，既然现在start~mid和mid+1~end这两段都分别有序，
+        // 那么对于start~mid中的任何一个元素，我们都可以轻易地知道在mid+1~end中有多少小于它的数
+        for (int i=start; i<=mid; i++)
+        {
+            int val = nums[i];
+            auto pos = lower_bound(sortedNums.begin()+mid+1, sortedNums.begin()+end+1,val);            
+            counts[i] += pos-(sortedNums.begin()+mid+1);
+        }
+        
+        // 将两段已经有序的数组段start~mid,mid+1~end合起来排序。
+        // 如果写归并排序的code会更快一些。这里就偷懒了，直接用sort函数。        
+        sort(sortedNums.begin()+start,sortedNums.begin()+end+1);        
+    }
+};
